05d6ea59c09d1171f8be082b0c7b13790d8b126c
Kamil Cieśla <kamil.ciesla@zendesk.com>
2023-01-19 21:17:57 +0100

    create Stage and Map component (#2)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Stage from './components/Stage';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <div className="stagesContainer">
+        <Stage></Stage>
+        <Stage></Stage>
+        <Stage></Stage>
+
+        </div>
+        <div className="mapContainer">
+          <Map></Map>
+        </div>
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,8 @@
+import React from "react";
+import '../styles/Map.css';
+
+function Map(){
+    return <div className="Map">
+        </div>
+}
+export default Map
