73464530d9fa0a1c9f9233e428ea752abc723804
Kamil Cieśla <kamilciesla34@gmail.com>
2023-05-21 15:12:30 +0200

    v0.0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+
+// Import react functions
 import { useState } from 'react';
 
+// Import app components
 import { Map } from './components/Map/Map'
 import { Time } from './components/Time/Time'
 import { Tripmates } from './components/Tripmates/Tripmates';
 
-import { Input, TextField, Card, CardContent, Typography } from '@mui/material';
+// Import MUI components
+import { Input, TextField, Card, Grid, CardContent, Typography } from '@mui/material';
+
+// Import images
+import planBgImage from './plan-bg.jpg'
+
+const planBgImageStyle = {
+  backgroundImage: `url(${planBgImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+}
 
 function App() {
   const exampleData = {
@@ -17,45 +30,100 @@ function App() {
   const [currency, setCurrency] = useState(exampleData.currency)
   const [tripmates, setTripmates] = useState(null)
 
-
   return (
     <div className="App">
       <div className="plan-container">
-        <Card className="plan-card" >
-          <CardContent sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '1rem'
+        <Card className="plan-card"
+          sx={{
+            height: '95vh'
+          }} >
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item lg={12}>
+                <div style={planBgImageStyle}>
+                  <div className="plan-title-container" style={{
+                    height: '10rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}>
+                    <Card>
+                      <CardContent >
+                        <Typography variant="h4">
+                          Trip to Greece
+                        </Typography>
+                        {/* <Input placeholder='Enter title for your trip'></Input> */}
+                      </CardContent>
+                    </Card>
+                  </div>
+                </div>
+              </Grid>
+              <Grid item lg={12}>
+                <TextField
+                  id="standard-multiline-flexible"
+                  label="Trip description"
+                  multiline
+                  rows={4}
+                />
+              </Grid>
+              <Grid item lg={12}>
+                <Time></Time>
+              </Grid>
+              <Grid item lg={12}>
+                <Tripmates></Tripmates>
+              </Grid>
+              <Grid item lg={12}>
+                <Card className="plan-cost">
+                  <CardContent>
+                    <Input placeholder='set up a budget for your trip'></Input>
+                    <Typography>
+                      Calculated cost of your trip: {totalCost} {currency}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item lg={12}>
+                <TextField
+                  id="standard-multiline-flexible"
+                  label="Trip notes"
+                  multiline
+                  rows={4}
+                />
+              </Grid>
+              <Grid item lg={12}>
+                <Card className="reservations">
+                  <CardContent>
+                    <Typography variant="h5">
+                      Reservations
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item lg={12}>
+                <Card className="documents">
+                  <CardContent>
+                    <Typography variant="h5">
+                      Documents
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item lg={12}>
 
-          }}>
-            <Typography variant="h3">
-              Travel planner
-            </Typography>
-            <Input placeholder='Enter title for your trip'></Input>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Trip description"
-              multiline
-              rows={4}
-            />
-            <Time></Time>
-            <Tripmates></Tripmates>
-            <Card className="plan-cost">
-              <CardContent>
-                <Input placeholder='set up a budget for your trip'></Input>
-                <Typography>
-                  Caluculated cost of your trip: {totalCost} {currency}
-                </Typography>
-              </CardContent>
-            </Card>
-
-
+                <Card className="Trip schedule">
+                  <CardContent>
+                    <Typography variant="h5">
+                      Trip schedule
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
           </CardContent>
-
         </Card>
       </div>
-      <Map>
-      </Map>
+      <Map></Map>
     </div >
   );
 }
---
+++ src/components/Photo/Photo.js
@@ -0,0 +1,18 @@
+// import Paper from 'material-paper'
+// import Image from './plan-bg.jpg'
+
+
+// const styles = {
+//     paperContainer: {
+//         height: 1356,
+//         backgroundImage: Image
+//     }
+// };
+
+// export const Photo = () => {
+//     return (
+//         <Paper style={styles.paperContainer}>
+//             Some text to fill the Paper Component
+//         </Paper>
+//     )
+// }
